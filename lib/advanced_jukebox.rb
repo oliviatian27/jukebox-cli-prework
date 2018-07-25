0cb627e8d2264630fe9983ae74e3acb4f4ec5cee
Olivia Tian <oliviatian27@gmail.com>
2018-07-25 14:32:24 +0000

    automatically backed up by learn

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -3,12 +3,12 @@
 #with the correct path to this directory on your computer
 
 my_songs = {
-"Go Go GO" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/01.mp3',
-"LiberTeens" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/02.mp3',
-"Hamburg" =>  '< path to this directory >/jukebox-cli/audio/Emerald-Park/03.mp3',
-"Guiding Light" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/04.mp3',
-"Wolf" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/05.mp3',
-"Blue" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/06.mp3',
+"Go Go GO" => 'home/oliviatian27/jukebox-cli-prework/audio/Emerald-Park/01.mp3',
+"LiberTeens" => 'home/oliviatian27/jukebox-cli-prework/audio/Emerald-Park/02.mp3',
+"Hamburg" =>  'home/oliviatian27/jukebox-cli-prework/audio/Emerald-Park/03.mp3',
+"Guiding Light" => 'home/oliviatian27/jukebox-cli-prework/audio/Emerald-Park/04.mp3',
+"Wolf" => 'home/oliviatian27/jukebox-cli-prework/audio/Emerald-Park/05.mp3',
+"Blue" => 'home/oliviatian27/jukebox-cli-prework/audio/Emerald-Park/06.mp3',
 "Graduation Failed" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/07.mp3'
 }
 
